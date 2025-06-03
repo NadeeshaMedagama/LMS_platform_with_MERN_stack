6997633dcc1bd23c6f4121a2b888703a71ec1d62
Nadeesha Medagama <nadeeshamedagama@gmail.com>
2025-06-04 01:19:06 +0530

    routing paths updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import theme from './theme';
+import Navbar from './components/layout/Navbar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import CoursesPage from './pages/CoursesPage';
+import CourseDetail from './pages/CourseDetail';
+import CreateCourse from './pages/CreateCourse';
+import PrivateRoute from './components/common/PrivateRoute';
+import AdminRoute from './components/common/AdminRoute';
+import InstructorRoute from './components/common/InstructorRoute';
+import WhyNovaLearn from './pages/WhyNovaLearn';
+import UseCases from './pages/UseCases';
+import Pricing from './pages/Pricing';
+import Resources from './pages/Resources';
+import JoinUs from "./pages/JoinUs";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Router>
+              <Navbar />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/courses" element={<CoursesPage />} />
+                <Route path="/why-novalearn" element={<WhyNovaLearn />} />
+                <Route path="/usecases" element={<UseCases />} />
+                <Route path="/pricing" element={<Pricing />} />
+                <Route path="/resources" element={<Resources />} />
+                <Route path="/joinus" element={<JoinUs />} />
+                <Route path="/courses/:id" element={<CourseDetail />} />
+
+                {/* Private Routes */}
+                <Route path="/dashboard" element={<PrivateRoute />}>
+                  <Route index element={<Dashboard />} />
+                </Route>
+
+                {/* Instructor Routes */}
+                <Route path="/create-course" element={<InstructorRoute />}>
+                  <Route index element={<CreateCourse />} />
+                </Route>
+
+                {/* Admin Routes */}
+                <Route path="/admin" element={<AdminRoute />}>
+                  <Route index element={<div>Admin Dashboard</div>} />
+                </Route>
+              </Routes>
+            </Router>
+          </ThemeProvider>
+        </PersistGate>
+      </Provider>
   );
 }
 
